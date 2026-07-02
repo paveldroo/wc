1a2f41c4f7ddc9bab308b8b75c0cc3b68fd73953
Pavel Druzhinin <paveldroo@gmail.com>
2026-07-02 19:48:45 +0300

    bytes count done

--- justfile
+++ justfile
@@ -1,4 +1,13 @@
 # Watch dev files
+run:
+    cargo run -- -c test.txt
+
+release:
+    cargo build --bin ccwc --release
+    cp target/release/ccwc .
+    chmod +x ./ccwc
+    ./ccwc -c test.txt
+
 watch:
     cargo watch -q -c -w src/ -x "run -- -c test.txt"
 
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ mod test_file;
 
 fn main() {
     let filename = args::parse_filename();
-    let content = test_file::get_test_data(filename);
-    dbg!(content);
+    let content = test_file::get_test_data(filename.clone());
+
+    let bytes_count = content.len();
+    println!("  {bytes_count:?} {filename}");
+    // dbg!(content);
 }
