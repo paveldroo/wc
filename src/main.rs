12b64d1f2937326153b0f4dec5ffa4a5b1150fa2
Pavel Druzhinin <paveldroo@gmail.com>
2026-07-03 00:10:41 +0300

    change module name

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod args;
-mod test_file;
+mod input;
 
 fn main() {
     let filename = args::parse_filename();
-    let content = test_file::get_test_data(filename.clone());
+    let content = input::get_test_data(filename.clone());
 
     let bytes_count = content.len();
     println!("  {bytes_count:?} {filename}");
